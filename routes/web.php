1137e57e02ff08023c67ee8f531d1532d1015cde
delladelianaduri <delladelianaduri12@gmail.com>
2024-05-23 21:02:43 +0700

    auth login register

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
+use Laravel\Lumen\Routing\Router;
 
+/** @var Router $router */
 $router->group(['prefix' => 'api'], function () use ($router) {
     // Route for user registration
-    $router->post('register', [AuthController::class, 'register']);
+    $router->post('register', ['uses' => 'AuthController@register']);
 
     // Route for user login
-    $router->post('login', [AuthController::class, 'login']);
+    $router->post('login', ['uses' => 'AuthController@login']);
 
-    // Other API routes can be defined here
+    // Route for fetching authenticated user data
+    $router->get('me', ['middleware' => 'auth', 'uses' => 'AuthController@me']);
 });
