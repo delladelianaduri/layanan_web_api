2c919e2cbd389c3ea039a2512b82df60ed3df3a1
delladelianaduri <delladelianaduri12@gmail.com>
2024-05-23 20:47:49 +0700

    auth login register

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,14 @@
 <?php
 
-/** @var \Laravel\Lumen\Routing\Router $router */
+use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+$router->group(['prefix' => 'api'], function () use ($router) {
+    // Route for user registration
+    $router->post('register', [AuthController::class, 'register']);
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+    // Route for user login
+    $router->post('login', [AuthController::class, 'login']);
 
-$router->post('/login', 'AuthController@login');
-$router->group(['middleware' => 'auth'], function () use ($router) {
-    $router->get('/profile', function () {
-        return response()->json(auth()->user());
-    });
+    // Other API routes can be defined here
 });
